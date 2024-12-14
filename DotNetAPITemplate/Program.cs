e407ddf072f4c3fd55319ebe0436cbf3711be0cc
Yusha Bakker <yushabakker1@gmail.com>
2024-12-14 20:16:44 +0100

    pushing more work for the template. assuirng a proper working template
    to do
    - connect to database
    - setup database
    - manual how to use it
    - manual how to setup controllers
    - manual how to setup databse
    - manual how to store database, and how to give to someone else

--- DotNetAPITemplate/Program.cs
+++ DotNetAPITemplate/Program.cs
@@ -12,9 +12,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+} else {
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
 
 var summaries = new[]
 {
